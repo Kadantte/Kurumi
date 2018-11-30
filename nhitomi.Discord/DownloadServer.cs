f0724a1f58ae1c9a6ae4f73b9b72e86567d8547f
phosphene47 <phosphene47@gmail.com>
2018-11-30 23:53:27 +1100

    Download implementation

--- nhitomi.Discord/DownloadServer.cs
+++ nhitomi.Discord/DownloadServer.cs
@@ -3,7 +3,11 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,21 +15,29 @@ namespace nhitomi
 {
     public class DownloadServer : IBackgroundService, IDisposable
     {
-        readonly AppSettings.HttpSettings _settings;
-        readonly HttpListener _http;
+        readonly AppSettings _settings;
+        readonly HttpClient _httpClient;
+        readonly ISet<IDoujinClient> _clients;
         readonly ILogger _logger;
+
+        public HttpListener HttpListener { get; }
 
         public DownloadServer(
             IOptions<AppSettings> options,
+            IHttpClientFactory httpFactory,
+            ISet<IDoujinClient> clients,
             ILogger<DownloadServer> logger
         )
         {
-            _settings = options.Value.Http;
-            _http = new HttpListener();
+            _settings = options.Value;
+            _httpClient = httpFactory.CreateClient(nameof(DownloadServer));
+            _clients = clients;
             _logger = logger;
 
-            var prefix = $"http://+:{_settings.Port}/";
-            _http.Prefixes.Add(prefix);
+            HttpListener = new HttpListener();
+
+            var prefix = $"http://+:{_settings.Http.Port}/";
+            HttpListener.Prefixes.Add(prefix);
 
             _logger.LogInformation($"HTTP listening at '{prefix}'.");
         }
@@ -34,7 +46,7 @@ namespace nhitomi
         {
             _logger.LogInformation($"Starting HTTP server.");
 
-            _http.Start();
+            HttpListener.Start();
 
             try
             {
@@ -43,7 +55,7 @@ namespace nhitomi
                 while (!token.IsCancellationRequested)
                 {
                     // Add new handlers
-                    while (handlerTasks.Count < _settings.Concurrency)
+                    while (handlerTasks.Count < _settings.Http.Concurrency)
                         handlerTasks.Add(HandleRequestAsync());
 
                     // Remove completed handlers
@@ -53,13 +65,13 @@ namespace nhitomi
                 // Wait for all handlers to finish
                 await Task.WhenAll(handlerTasks);
             }
-            finally { _http.Stop(); }
+            finally { HttpListener.Stop(); }
         }
 
         public async Task HandleRequestAsync()
         {
             // Listen for request
-            var context = await _http.GetContextAsync();
+            var context = await HttpListener.GetContextAsync();
 
             switch (context.Request.HttpMethod)
             {
@@ -75,29 +87,66 @@ namespace nhitomi
             context.Response.Close();
         }
 
+        static Regex _dlRegex = new Regex(@"^\/dl\/(?<token>.+\..+)$", RegexOptions.Compiled);
+
         public async Task HandleGetAsync(HttpListenerRequest request, HttpListenerResponse response)
         {
-            switch (request.Url.AbsolutePath)
+            var path = request.Url.AbsolutePath;
+
+            if (path == "/")
             {
-                case "/":
-                    const string index_txt = @"
+                const string index_txt = @"
 nhitomi - Discord doujinshi bot by phosphene47#7788
 
 - Discord: https://discord.gg/bf3q7RM
 - Github: https://github.com/phosphene47/nhitomi";
-                    using (var writer = new StreamWriter(response.OutputStream))
-                    {
-                        await writer.WriteAsync(index_txt.Trim());
-                        await writer.FlushAsync();
-                    }
-                    break;
+                using (var writer = new StreamWriter(response.OutputStream))
+                {
+                    await writer.WriteAsync(index_txt.Trim());
+                    await writer.FlushAsync();
+                }
 
-                default:
-                    response.StatusCode = (int)HttpStatusCode.NotFound;
-                    break;
+                return;
             }
+            if (path.StartsWith("/dl/"))
+            {
+                var match = _dlRegex.Match(path);
+                var token = match.Groups.FirstOrDefault(g => g.Success && g.Name == "token")?.Value;
+
+                // Parse token
+                if (token != null &&
+                    !TokenGenerator.TryDeserializeToken(
+                        token: token,
+                        secret: _settings.Discord.Token,
+                        sourceName: out var sourceName,
+                        id: out var id
+                    ))
+                {
+                    // Retrieve doujin
+                    var client = _clients.First(c => c.Name == sourceName);
+                    var doujin = await client.GetAsync(id);
+
+                    // Send zip to client
+                    using (var zip = new ZipArchive(response.OutputStream, ZipArchiveMode.Create, leaveOpen: true))
+                    {
+                        foreach (var pageUrl in doujin.PageUrls)
+                        {
+                            // Create file in zip
+                            var entry = zip.CreateEntry(Path.GetFileName(pageUrl), CompressionLevel.Optimal);
+
+                            // Write page contents to entry
+                            using (var dst = entry.Open())
+                            using (var src = await _httpClient.GetStreamAsync(pageUrl))
+                                await src.CopyToAsync(dst);
+                        }
+                    }
+                    return;
+                }
+            }
+
+            response.StatusCode = (int)HttpStatusCode.NotFound;
         }
 
-        public void Dispose() => _http.Close();
+        public void Dispose() => HttpListener.Close();
     }
 }
