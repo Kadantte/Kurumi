3d229708fda5027a4f872efde2d350ca98e72f83
phosphene47 <phosphene47@gmail.com>
2019-01-04 22:13:32 +0900

    Order by id lastly

--- nhitomi.Discord/HitomiClient.cs
+++ nhitomi.Discord/HitomiClient.cs
@@ -336,6 +336,7 @@ namespace nhitomi
                     .OrderByDescending(d => matches(d.name))
                     .ThenByDescending(d => matches(d.author))
                     .ThenByDescending(d => matchTags(d.tags))
+                    .ThenByDescending(d => d.id)
                     .TakeWhile(d => matches(d.name) > 0 && matches(d.author) > 0 && matchTags(d.tags) > 0)
                     .Select(d => d.id);
             }
