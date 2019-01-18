4d238b7f53d2fec9f7cb29063e7bcbb1749a4f99
phosphene47 <phosphene47@gmail.com>
2019-01-18 17:31:33 +0900

    Implement get api endpoint

--- nhitomi/ApiController.cs
+++ nhitomi/ApiController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace nhitomi
@@ -5,6 +8,22 @@ namespace nhitomi
     [Route("api")]
     public class ApiController : ControllerBase
     {
+        readonly ISet<IDoujinClient> _clients;
 
+        public ApiController(
+            ISet<IDoujinClient> clients
+        )
+        {
+            _clients = clients;
+        }
+
+        [HttpGet("{source}/{id}")]
+        public async Task<IDoujin> GetDoujinAsync(string source, string id)
+        {
+            // Find client by name
+            var client = _clients.First(c => c.Name == source);
+
+            return await client.GetAsync(id);
+        }
     }
 }
