b65845a5908bb27ec83f87b65b15b8d54806544d
phosphene47 <phosphene47@gmail.com>
2019-02-08 18:44:39 +1100

    Remove Microsoft cache namespace using

--- nhitomi/Startup.cs
+++ nhitomi/Startup.cs
@@ -7,7 +7,6 @@ using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
