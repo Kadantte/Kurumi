c7200c9a7285a033cb0b82211cae836708303a99
phosphene47 <phosphene47@gmail.com>
2019-03-11 14:12:57 +1100

    Add DefaultController for nhitomi.Proxy

---
+++ nhitomi.Proxy/DefaultController.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2018-2019 phosphene47
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace nhitomi.Proxy
+{
+    [Route("/")]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "nhitomi proxy server";
+    }
+}
