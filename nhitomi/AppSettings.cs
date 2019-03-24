e30fde8361deae32a171771b81525a1eeec10916
phosphene47 <phosphene47@gmail.com>
2019-03-24 12:11:42 +1100

    Remove unused ItemsPerPage

--- nhitomi/AppSettings.cs
+++ nhitomi/AppSettings.cs
@@ -60,7 +60,6 @@ namespace nhitomi
         {
             public int Port { get; set; }
             public string Url { get; set; }
-            public int ItemsPerPage { get; set; }
 
             public HttpSettings()
             {
