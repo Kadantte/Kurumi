11416900b547c0399d5a2c67c9539596d6807aef
phosphene47 <phosphene47@gmail.com>
2019-03-24 19:11:47 +1100

    Add license header in DownloadProxyManager

--- nhitomi/DownloadProxyManager.cs
+++ nhitomi/DownloadProxyManager.cs
@@ -1,3 +1,8 @@
+// Copyright (c) 2019 phosphene47
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
