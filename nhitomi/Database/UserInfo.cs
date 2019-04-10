fe871dda85fd90ec7f7c25c9d4a96dd4b3e646c5
phosphene47 <phosphene47@gmail.com>
2019-04-10 21:58:28 +1000

    Update UserInfo

--- nhitomi/Database/UserInfo.cs
+++ nhitomi/Database/UserInfo.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using Amazon.DynamoDBv2.DataModel;
 
 namespace nhitomi.Database
@@ -6,9 +5,5 @@ namespace nhitomi.Database
     public class UserInfo
     {
         [DynamoDBHashKey("userId")] public ulong UserId { get; set; }
-
-        // dict (collectionName, dict (source/id, doujinName))
-        [DynamoDBProperty("collections")]
-        public Dictionary<string, Dictionary<string, string>> Collections { get; set; }
     }
 }
